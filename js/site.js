ee08257407d9dac3f8cb8e8cb69f0469f4816dbe
Steve <acahir@users.noreply.github.com>
2019-01-27 00:27:49 -0500

    Update project website to initial Barebones release (v3.0.0)
    Significant changes throughout branch:
    index.html modified to
    	- document all usage and technical changes
    	- structurally changes to use Barebones from Skeleton
    orig index.html from skeleton maintained as examples/skeleton.html
    	- test backwards compatibility
    	- demonstrate how to use legacy support
    site.js
    	- completely re-written to remove dependence on jquery
    	- polyfill for scrolling only external dependency
    
    Other changes were in support of other changes moving from Skeleton to
    Barebones or just modernizing (updating normalize, adding favicon sizes, etc)

--- js/site.js
+++ js/site.js
@@ -1,94 +1,132 @@
-$(document).ready(function() {
+document.addEventListener("DOMContentLoaded", function() {
 
   // Variables
-  var $codeSnippets = $('.code-example-body'),
-      $nav = $('.navbar'),
-      $body = $('body'),
-      $window = $(window),
-      $popoverLink = $('[data-popover]'),
-      navOffsetTop = $nav.offset().top,
-      $document = $(document),
-      entityMap = {
-        "&": "&amp;",
-        "<": "&lt;",
-        ">": "&gt;",
-        '"': '&quot;',
-        "'": '&#39;',
-        "/": '&#x2F;'
-      }
+  var	navOffsetTop = document.querySelector('.navbar').getBoundingClientRect().top + document.body.scrollTop;
+	var	entityMap = {
+					"&": "&amp;",
+					"<": "&lt;",
+					">": "&gt;",
+					'"': '&quot;',
+					"'": '&#39;',
+					"/": '&#x2F;'
+				};
 
-  function init() {
-    $window.on('scroll', onScroll)
-    $window.on('resize', resize)
-    $popoverLink.on('click', openPopover)
-    $document.on('click', closePopover)
-    $('a[href^="#"]').on('click', smoothScroll)
+  function init() {  	
+  	// handle menu popovers
+  	var y = document.querySelectorAll('[data-popover]')
+  	y.forEach(function(e) {
+  		e.addEventListener('click', newOpenPopover)
+  	})
+  	document.addEventListener('click', newClosePopover)
+
+		// attach click even to all anchor links
+		addSmoothScroll();
+		
+		// add click event to theme buttons
+		var x = document.querySelector('.themeButtons')
+		x.childNodes.forEach(function(e) {
+			e.addEventListener('click', toggleTheme)
+		})
+		
+		// listener for navbar visibility
+    window.addEventListener('scroll', onScroll);
+    
+    // prep for html escaping
     buildSnippets();
   }
 
-  function smoothScroll(e) {
-    e.preventDefault();
-    $(document).off("scroll");
-    var target = this.hash,
-        menu = target;
-    $target = $(target);
-    $('html, body').stop().animate({
-        'scrollTop': $target.offset().top-40
-    }, 0, 'swing', function () {
-        window.location.hash = target;
-        $(document).on("scroll", onScroll);
-    });
-  }
-
-  function openPopover(e) {
-    e.preventDefault()
-    closePopover();
-    var popover = $($(this).data('popover'));
-    popover.toggleClass('open')
-    e.stopImmediatePropagation();
-  }
-
-  function closePopover(e) {
-    if($('.popover.open').length > 0) {
-      $('.popover').removeClass('open')
-    }
-  }
-
-  $("#button").click(function() {
-    $('html, body').animate({
-        scrollTop: $("#elementtoScrollToID").offset().top
-    }, 2000);
-});
-
-  function resize() {
-    $body.removeClass('has-docked-nav')
-    navOffsetTop = $nav.offset().top
-    onScroll()
-  }
-
-  function onScroll() {
-    if(navOffsetTop < $window.scrollTop() && !$body.hasClass('has-docked-nav')) {
-      $body.addClass('has-docked-nav')
-    }
-    if(navOffsetTop > $window.scrollTop() && $body.hasClass('has-docked-nav')) {
-      $body.removeClass('has-docked-nav')
-    }
-  }
-
+  /* non-jquery functions */
+	
+	function toggleTheme(e) {
+		var target = e.target
+		// check if button pressed was already primary
+		if (!target.classList.contains('button-primary')) {
+			// get sibling, which might be before
+			var sib = target.nextElementSibling || target.previousElementSibling
+			target.classList.add('button-primary')
+			sib.classList.remove('button-primary')
+			
+			var prettifyDark = document.getElementById('prettify-dark')
+			var sheets = document.styleSheets
+			
+			// temporariily set transition class on html element
+			document.documentElement.classList.add('transition-theme')
+			
+			// change data-theme
+			if (target.classList.contains('darkTheme')) {
+				document.documentElement.setAttribute("data-theme", "dark")
+				prettifyDark.removeAttribute('disabled')
+				prettifyDark.disabled = false
+			} else {
+				document.documentElement.removeAttribute("data-theme", "dark")
+				prettifyDark.disabled = true
+			}
+			
+			// remove transition class
+			window.setTimeout(function() {
+				document.documentElement.classList.remove('transition-theme')
+			}, 1000)
+		}
+	}
+	
+	function addSmoothScroll() {
+		var els = document.querySelectorAll('[href^="#"]')
+		els.forEach(function(e) {
+			e.addEventListener('click', function(i) {
+				i.preventDefault()
+				var target = e.getAttribute('href')
+				document.getElementById(target.substring(1)).scrollIntoView({ behavior: 'smooth' });
+			});
+		});
+	}
+	
   function escapeHtml(string) {
     return String(string).replace(/[&<>"'\/]/g, function (s) {
       return entityMap[s];
     });
   }
-
+  
   function buildSnippets() {
-    $codeSnippets.each(function() {
-      var newContent = escapeHtml($(this).html())
-      $(this).html(newContent)
-    })
+  	var els = document.querySelectorAll('.code-example-body')
+  	els.forEach(function(e) {
+      var newContent = escapeHtml(e.innerHTML)
+      e.innerHTML = newContent
+  	})
+  }
+  
+  function newOpenPopover(e) {
+    e.preventDefault()
+    newClosePopover();
+    var pop = e.target.getAttribute('data-popover')
+    var popoverEl = document.getElementById(pop)
+    //var popover = $($(this).data('popover'));
+    popoverEl.classList.add('open')
+    e.stopImmediatePropagation();
   }
 
+  function newClosePopover(e) {
+  	var x = document.querySelector('.popover.open')
+    if(x != null) {
+      x.classList.remove('open')
+    }
+  }
+  
+  window.onresize = function() {
+  	document.body.classList.remove('has-docked-nav')
+  	var rect = document.querySelector('.navbar').getBoundingClientRect();
+  	navOffsetTop = rect.top + document.body.scrollTop
+  	onScroll()
+  }
+
+	function onScroll() {
+  	var b = document.body
+    if(navOffsetTop < window.scrollY && !b.classList.contains('has-docked-nav')) {
+      b.classList.add('has-docked-nav')
+    }
+    if(navOffsetTop > window.scrollY && b.classList.contains('has-docked-nav')) {
+      b.classList.remove('has-docked-nav')
+    }
+  }
 
   init();
-
 });
